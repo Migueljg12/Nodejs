05c92ff66f74585319c326b0b07b430659f37e8d
Miguel Gonçalves <79999297+Migueljg12@users.noreply.github.com>
2021-08-09 19:04:57 -0300

    Delete MYSITE directory

--- MYSITE/controllers/index.js
+++
@@ -1,5 +0,0 @@
-import UserController from './user.js'
-import AuthController from './auth.js'
-import PostController from './post.js'
-
-export default [UserController, AuthController, PostController]
--- MYSITE/services/index.js
+++
@@ -1,4 +0,0 @@
-import UserService from './users.js'
-import PostService from './posts.js'
-
-export { UserService, PostService }
