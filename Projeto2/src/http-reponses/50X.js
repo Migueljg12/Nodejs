c3073649fd36d506029b57a661ba60b678a15071
Miguel Gonçalves <79999297+Migueljg12@users.noreply.github.com>
2021-08-09 19:02:20 -0300

    Delete Projeto2 directory

--- Projeto2/src/http-reponses/50X.js
+++
@@ -1,9 +0,0 @@
-class InternalServerError extends Error {
-  constructor() {
-    super()
-    this.name = 'Internal Server Error'
-    this.statusCode = 500
-  }
-}
-
-module.exports = InternalServerError
--- Projeto2/src/http-reponses/GeneralError.js
+++
@@ -1,3 +0,0 @@
-class GeneralError extends Error { }
-
-module.exports = GeneralError
